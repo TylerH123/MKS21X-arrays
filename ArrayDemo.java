b2ecd366ed0aecacfa685a8604c05bf4e2ce27e3
Tyler Huang <thuang00@stuy.edu>
2018-09-26 21:25:29 -0400

    ArrayDemo.java

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -14,7 +14,31 @@ public class ArrayDemo{
 		}
 		return count;
 	}
-	
+	public static void fill2D(int[][] vals){
+		for (int i = 0; i < vals.length; i++){
+			for (int j = 0; j< vals[i].length; j++){
+				if (i == j){
+					vals[i][j] = 3;
+				}
+				else{
+					vals[i][j] = 1;
+				}
+			}
+		}
+	}
+	public static int[][] fill2DCopy(int[][] vals){
+		int[][] newVals = new int[vals.length][vals[].length];
+		for (int i = 0; i < newVals.length; i++){
+			for (int j = 0; j< newVals[i].length; j++){
+				if (newVals[i][j] < 0){
+					newVals[i][j] = 3;
+				}
+				else{
+					newVals[i][j] = 1;
+				}
+			}
+		}
+	}
 	public static void main(String [] args){
 	}
 }
