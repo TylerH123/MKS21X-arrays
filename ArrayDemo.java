40f51b2174ce199519dca0ab09f49a1c42e70e1a
Tyler Huang <thuang00@stuy.edu>
2018-09-27 21:28:48 -0400

    ArrayDemo.java

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,15 +1,24 @@
 public class ArrayDemo{
+	public static String stringArr(int[] arr){
+		String array = "[";
+    for(int i = 0; i < arr.length-1; i++){
+      array = array + arr[i] + ", ";
+    }
+    array += arr[arr.length - 1] + "]";
+    return array;
+	}
+	public static String stringArr2(int[][] arr){
+	  String array = "[";
+	  for(int i = 0; i < arr.length; i++){
+	    array += stringArr(arr[i]);
+	  }
+	  return array + "]";
+	}
 	public static void printArray(int[]ary){
-		for (int i = 0; i < ary.length; i++){
-			System.out.println(ary[i]);
-		}
+		System.out.println(stringArr(ary));
 	}
 	public static void printArray(int[][] ary){
-		for (int i = 0; i < ary.length; i++){
-			for (int j = 0; j < ary[i].length; j++){
-				System.out.println(ary[i][j]);
-			}
-		}
+		System.out.println(stringArr2(ary));
 	}
 	public static int countZeros2D(int[][] nums){
 		int count = 0;
@@ -53,10 +62,10 @@ public class ArrayDemo{
 		int[] arr1 = new int[] {0,1,2,3};
 		int[][] arr2 = new int[][] {{0,1,2},{0,0,0}};
 	  int[][] arr3 = new int[][] {{0,1,2},{0,4,5},{9,7,6}};
-		//printArray(arr1);
-		//printArray(arr2);
-    //System.out.println(countZeros2D(arr2));
-		//fill2D(arr3);
+		printArray(arr1);
+		printArray(arr2);
+    System.out.println(countZeros2D(arr2));
+		fill2D(arr3);
     System.out.println(fill2DCopy(arr3));
 	}
 }
